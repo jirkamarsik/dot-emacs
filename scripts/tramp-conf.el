e4c407b7dada4fede98da7688211e9bbb4031fe8
Jiri Marsik <jiri.marsik89@gmail.com>
2012-06-04 17:48:32 +0800

    Renamed tramp.el to tramp-conf.el so Emacs doesn't confuse it with the real TRAMP

--- scripts/tramp-conf.el
+++ scripts/tramp-conf.el
@@ -1,5 +1,6 @@
 ;; Make Tramp store autosaves in the backups folder.
-(setq tramp-auto-save-directory "/home/jirka/.emacs.d/backups/tramp")
+(eval-after-load "tramp"
+  '(setq tramp-auto-save-directory "/home/jirka/.emacs.d/backups/tramp"))
 
 ;; Check out http://www.rekk.de/bloggy/emacs-and-tramp/ and experiment
 ;; with this (is the above enough? should I do this on-loading?)
